a044896cc8c27c1ac4f0e3eb3e75ef4cf7769948
Kumaran Chandrashekar <k31311558@gmail.com>
2026-06-11 11:16:53 +0530

    Delete analysis directory

--- analysis/risk_score.py
+++
@@ -1,9 +0,0 @@
-import json
-with open("data/accounts.json") as f:
-    accounts = json.load(f)
-for account in accounts:
-    velocity = account["velocity"]
-    connections = account["connections"]
-    score = (velocity * 0.6) + (connections * 0.4)
-    account["risk"] = min(100, round(score))
-print(accounts)
